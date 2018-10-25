68df0d508b20c6bf270b5a125ba5d0bf42a55577
Genta Kamitani <oftn.mofumofu@gmail.com>
2018-10-25 19:33:17 +0900

    fix bug for nested require_dependencies

--- padrino-core/lib/padrino-core/reloader/storage.rb
+++ padrino-core/lib/padrino-core/reloader/storage.rb
@@ -18,12 +18,13 @@ module Padrino
         files.delete(name)
       end
 
-      def prepare(name)
+      def prepare(name, mtimes)
         file = remove(name)
         @old_entries ||= {}
         @old_entries[name] = {
-          :constants => ObjectSpace.classes,
-          :features  => old_features = Set.new($LOADED_FEATURES.dup)
+          :constants => object_classes,
+          :features  => old_features = Set.new($LOADED_FEATURES.dup),
+          :mtimes => mtimes.dup
         }
         features = file && file[:features] || []
         features.each{ |feature| Reloader.safe_load(feature, :force => true) }
@@ -39,8 +40,10 @@ module Padrino
         @old_entries.delete(name)
       end
 
-      def rollback(name)
-        new_constants = ObjectSpace.new_classes(@old_entries[name][:constants])
+      def rollback(name, mtimes)
+        new_constants = new_classes(@old_entries[name][:constants]).reject do |constant|
+          newly_commited_constant?(constant, mtimes, @old_entries[name][:mtimes])
+        end
         new_constants.each{ |klass| Reloader.remove_constant(klass) }
         @old_entries.delete(name)
       end
@@ -49,6 +52,54 @@ module Padrino
 
       def files
         @files ||= {}
+      end
+
+      ##
+      # Returns all the classes in the object space.
+      #
+      def object_classes
+        klasses = Set.new
+
+        ObjectSpace.each_object(::Class).each do |klass|
+          if block_given?
+            if filtered_class = yield(klass)
+              klasses << filtered_class
+            end
+          else
+            klasses << klass
+          end
+        end
+
+        klasses
+      end
+
+      ##
+      # Returns a list of object space classes that are not included in "snapshot".
+      #
+      def new_classes(snapshot)
+        object_classes do |klass|
+          snapshot.include?(klass) ? nil : klass
+        end
+      end
+
+      ##
+      # Returns true if and only if constant is commited after prepare
+      #
+      def newly_commited_constant?(constant, mtimes, old_mtimes)
+        newly_commited_files(files, mtimes, old_mtimes).each do |_, entry|
+          return true if entry[:constants].include?(constant)
+        end
+        false
+      end
+
+      ##
+      # Returns a list of entries in "files" that is commited after prepare
+      #
+      def newly_commited_files(files, mtimes, old_mtimes)
+        files.select do |file, _|
+          next true unless old_mtimes[file]
+          old_mtimes[file] < mtimes[file]
+        end
       end
     end
   end
