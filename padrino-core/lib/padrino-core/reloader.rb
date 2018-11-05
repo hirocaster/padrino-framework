bfd045a0812562b81a43d3c9eab37a24ad307520
hirocaster <hohtsuka@gmail.com>
2018-11-05 20:48:59 +0900

    Remove MTIMES cache at remove_constant in reloader
    Remove MTIMES cache at remove_constant in reloader,
    so will re-require by dependencies.

--- padrino-core/lib/padrino-core/reloader.rb
+++ padrino-core/lib/padrino-core/reloader.rb
@@ -138,6 +138,13 @@ module Padrino
       @special_files = Set.new(files)
     end
 
+    ###
+    # Macro for mtime remove.
+    #
+    def remove_modification_time(file)
+      MTIMES.delete(file)
+    end
+
     private
 
     ##
